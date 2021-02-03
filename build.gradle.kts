33432619d6336cd43ebf46fadb9de12f01e4d136
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-02-03 18:33:20 +0100

    SubKt: bump to v0.1.15, properties/build updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-
+import myaa.subkt.ass.*
 import myaa.subkt.tasks.*
 import myaa.subkt.tasks.Mux.*
 import myaa.subkt.tasks.Nyaa.*
-import myaa.subkt.ass.EventLineAccessor
 import java.awt.Color
 import java.time.*
 
@@ -10,22 +9,53 @@ plugins {
     id("myaa.subkt")
 }
 
+fun String.isKaraTemplate(): Boolean {
+	return this.startsWith("code") || this.startsWith("template") || this.startsWith("mixin")
+}
+
+fun EventLine.isKaraTemplate(): Boolean {
+	return this.comment && this.effect.isKaraTemplate()
+}
+
+
 subs {
     readProperties("sub.properties")
     episodes(getList("episodes"))
+
+	val op_ktemplate by task<Automation> {
+        if (propertyExists("OP")) {
+            from(get("OP"))
+        }
+
+        video(get("premux"))
+		script("0x.KaraTemplater.moon")
+		macro("0x539's Templater")
+		loglevel(Automation.LogLevel.WARNING)
+	}
+
+    val ed_ktemplate by task<Automation> {
+        if (propertyExists("ED")) {
+            from(get("ED"))
+        }
+
+        video(get("premux"))
+		script("0x.KaraTemplater.moon")
+		macro("0x539's Templater")
+		loglevel(Automation.LogLevel.WARNING)
+	}
 
     merge {
         from(get("dialogue"))
 
         if (propertyExists("OP")) {
-            from(get("OP")) {
-                syncTo(getAs<Duration>("opsync"))
+            from(op_ktemplate.item()) {
+                syncTargetTime(getAs<Duration>("opsync"))
             }
         }
 
         if (propertyExists("ED")) {
-            from(get("ED")) {
-                syncTo(getAs<Duration>("edsync"))
+            from(ed_ktemplate.item()) {
+                syncTargetTime(getAs<Duration>("edsync"))
             }
         }
 
@@ -33,46 +63,60 @@ subs {
             from(get("IS"))
         }
 
-        if (propertyExists("TS")) {
-            from(get("TS"))
-        }
+        from(getList("TS"))
 
         out(get("mergedname"))
     }
+
+	val cleanmerge by task<ASS> {
+		from(merge.item())
+    	ass {
+			events.lines.removeIf { it.isKaraTemplate() }
+	    }
+	}
 
     chapters {
         from(merge.item())
         chapterMarker("chapter")
     }
 
+
     mux {
-        from(get("premux")) {
+        title(get("title"))
+
+		from(get("premux")) {
+			video {
+				lang("jpn")
+				default(true)
+			}
+			audio {
+				lang("jpn")
+				default(true)
+			}
             includeChapters(false)
 			attachments { include(false) }
-        }
+		}
 
-		from(merge.item()) {
+		from(cleanmerge.item()) {
 			tracks {
 				lang("eng")
                 name(get("group"))
 				default(true)
-				forced(false)
+				forced(true)
 				compression(CompressionType.ZLIB)
 			}
 		}
 
         chapters(chapters.item()) { lang("eng") }
 
+        attach(get("common_fonts")) {
+            includeExtensions("ttf", "otf")
+        } // every ep has an OP/ED, so can just add their font to this
+
         attach(get("fonts")) {
             includeExtensions("ttf", "otf")
         }
 
         out(get("muxout"))
-    }
-
-    torrent {
-        from(mux.item())
-        trackers(getList("trackers"))
-        out(get("single_ep"))
     }
 }
